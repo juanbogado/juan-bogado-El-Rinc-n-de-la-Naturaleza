2acce6dd2b5e1e397998cd5f0c6258f637fc1c56
Juanbogado <bogado.jmanuel@gmail.com>
2023-10-31 15:10:49 -0300

    preentrega1-bogado-juan

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarComponents from './components/NavBarComponents/NavBarComponents';
+
+function App() {
+  
+  return <div>
+    <NavBarComponents/>
+  </div>;
+}
+
+export default App
---
+++ src/components/NavBarComponents/NavBarComponents.jsx
@@ -0,0 +1,31 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const NavBarComponents = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">El Rincón de la Naturaleza</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Productos</Nav.Link>
+            <NavDropdown title="Categorías" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Semillas</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+              Herramientas de jardinería
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Sustratos y abonos</NavDropdown.Item>
+              <NavDropdown.Divider />
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBarComponents;
